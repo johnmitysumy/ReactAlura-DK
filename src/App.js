d2fa8c9a1dcb9a611d1565cc64ae8803b0834d4b
JohnMitysumy <johnmitysumy@gmail.com>
2023-04-10 18:14:40 -0300

    Atualização Banner banner atualizado, criado uma margem em cima e em baixo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
-import ListaSuspensa from './componentes/ListaSuspensa';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+  {
+    nome: 'Comandantes',
+    corPrimaria: '#5c5c5c',
+    corSecundaria: '#888686'
+  },
+  {
+    nome: 'Cabos',
+    corPrimaria: '#5c5c5c',
+    corSecundaria: '#979595'
+  },
+  {
+    nome: 'Soldados',
+    corPrimaria: '#5c5c5c',
+    corSecundaria: '#b9b6b6'
+  },
+  ]
+
   return (
     <div className="App">
       <Banner />
+
+      {times.map(times => <Time key={times.nome} nome={times.nome} corPrimaria={times.corPrimaria} corSecundaria={times.corSecundaria}/>)}
+
       <Formulario />
     </div>
   );
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
@@ -9,14 +10,21 @@ const Formulario = () => {
         'Cabo'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Solicitação enviada')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha o formulário para entrar para o clã DK</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Email" placeholder="Digite seu Email" />
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da sua imagem" />
-                <ListaSuspensa label="Patente" itens={times}/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome (identificação)" />
+                <CampoTexto obrigatorio={true} label="Email" placeholder="Digite seu Email" />
+                <CampoTexto obrigatorio={true} label="Idade" placeholder="Digite a sua idade" />
+                <Botao>
+                    Enviar Solicitação
+                </Botao>
             </form>
         </section>
     )
